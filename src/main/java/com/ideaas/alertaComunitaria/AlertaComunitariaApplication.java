503c0fc170b89fb46206190d592290fdb4962227
fedeberon <Mendoza2020>
2020-05-26 16:16:22 -0300

    First Commit

---
+++ src/main/java/com/ideaas/alertaComunitaria/AlertaComunitariaApplication.java
@@ -0,0 +1,13 @@
+package com.ideaas.alertaComunitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertaComunitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertaComunitariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideaas/alertaComunitaria/AlertaComunitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideaas.alertaComunitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaComunitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
